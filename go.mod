97edee705d22d9a73ab96a158befc4836849383b
zhourong <zhourong@hyperchain.cn>
2021-04-21 15:32:08 +0800

    feat(*): add bloom

--- go.mod
+++ go.mod
@@ -4,14 +4,11 @@ go 1.13
 
 require (
 	github.com/cbergoon/merkletree v0.2.0
+	github.com/ethereum/go-ethereum v1.10.2
 	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
 	github.com/gogo/protobuf v1.3.1
-	github.com/golang/protobuf v1.4.0 // indirect
-	github.com/golang/snappy v0.0.2-0.20200707131729-196ae77b8a26 // indirect
-	github.com/hashicorp/golang-lru v0.5.4 // indirect
-	github.com/jackpal/go-nat-pmp v1.0.2-0.20160603034137-1fa385a6f458 // indirect
+	github.com/golang/protobuf v1.4.3
 	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
-	github.com/jonboulle/clockwork v0.1.0 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
 	github.com/lestrrat-go/file-rotatelogs v2.2.0+incompatible
@@ -21,19 +18,13 @@ require (
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-isatty v0.0.10 // indirect
 	github.com/multiformats/go-multiaddr v0.2.0
-	github.com/onsi/ginkgo v1.11.0 // indirect
-	github.com/onsi/gomega v1.8.1 // indirect
-	github.com/opentracing/opentracing-go v1.1.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/tsdb v0.10.0
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/sirupsen/logrus v1.5.0
 	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271
-	github.com/stretchr/testify v1.4.0
-	github.com/syndtr/goleveldb v1.0.1-0.20190923125748-758128399b1d
+	github.com/stretchr/testify v1.7.0
+	github.com/syndtr/goleveldb v1.0.1-0.20210305035536-64b5b1c73954
 	github.com/tebeka/strftime v0.1.3 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
-	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- types/types.go
+++ types/types.go
@@ -2,20 +2,29 @@ package types
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"sync"
 
 	mt "github.com/cbergoon/merkletree"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/golang/protobuf/jsonpb"
 	"golang.org/x/crypto/sha3"
 )
 
+// hasherPool holds LegacyKeccak256 hashers for rlpHash.
+var hasherPool = sync.Pool{
+	New: func() interface{} { return sha3.NewLegacyKeccak256() },
+}
+
 // Lengths of hashes and addresses in bytes.
 const (
-	HashLength    = 32
-	AddressLength = 20
+	HashLength      = 32
+	AddressLength   = 20
+	BloomByteLength = 256
 )
 
 type Hash struct {
@@ -27,6 +36,8 @@ type Address struct {
 	RawAddress [AddressLength]byte `json:"raw_address"`
 	Address    string              `json:"address"`
 }
+
+type Bloom [BloomByteLength]byte
 
 func (h *Hash) MarshalJSONPB(m *jsonpb.Marshaler) ([]byte, error) {
 	return h.MarshalJSON()
@@ -318,4 +329,54 @@ func IsValidAddressByte(data []byte) bool {
 		return false
 	}
 	return true
+}
+
+func (b *Bloom) ProtoMessage() {}
+
+func (b *Bloom) Size() int {
+	return BloomByteLength
+}
+
+func (b *Bloom) MarshalTo(data []byte) (int, error) {
+	data = data[:b.Size()]
+	copy(data, b[:])
+
+	return b.Size(), nil
+}
+
+func (b *Bloom) Unmarshal(data []byte) error {
+	if len(data) > BloomByteLength {
+		data = data[len(data)-BloomByteLength:]
+	}
+
+	copy(b[:], data)
+
+	return nil
+}
+
+// Test checks if the given topic is present in the bloom filter
+func (b Bloom) Test(topic []byte) bool {
+	i1, v1, i2, v2, i3, v3 := bloomValues(topic, make([]byte, 6))
+	return v1 == v1&b[i1] &&
+		v2 == v2&b[i2] &&
+		v3 == v3&b[i3]
+}
+
+// bloomValues returns the bytes (index-value pairs) to set for the given data
+func bloomValues(data []byte, hashbuf []byte) (uint, byte, uint, byte, uint, byte) {
+	sha := hasherPool.Get().(crypto.KeccakState)
+	sha.Reset()
+	sha.Write(data)
+	sha.Read(hashbuf)
+	hasherPool.Put(sha)
+	// The actual bits to flip
+	v1 := byte(1 << (hashbuf[1] & 0x7))
+	v2 := byte(1 << (hashbuf[3] & 0x7))
+	v3 := byte(1 << (hashbuf[5] & 0x7))
+	// The indices for the bytes to OR in
+	i1 := BloomByteLength - uint((binary.BigEndian.Uint16(hashbuf)&0x7ff)>>3) - 1
+	i2 := BloomByteLength - uint((binary.BigEndian.Uint16(hashbuf[2:])&0x7ff)>>3) - 1
+	i3 := BloomByteLength - uint((binary.BigEndian.Uint16(hashbuf[4:])&0x7ff)>>3) - 1
+
+	return i1, v1, i2, v2, i3, v3
 }
