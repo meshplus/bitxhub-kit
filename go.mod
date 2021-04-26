d619c160bd0baf102c0f205df268b68f2600b308
zhourong <zhourong@hyperchain.cn>
2021-04-26 18:11:33 +0800

    fix(*): add MarshalText for Bloom

--- go.mod
+++ go.mod
@@ -18,7 +18,6 @@ require (
 	github.com/libp2p/go-libp2p-core v0.3.0
 	github.com/mattn/go-colorable v0.1.4 // indirect
 	github.com/mattn/go-isatty v0.0.10 // indirect
-	github.com/meshplus/bitxhub-model v1.1.1
 	github.com/multiformats/go-multiaddr v0.2.0
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/prometheus/tsdb v0.10.0
--- types/types.go
+++ types/types.go
@@ -10,6 +10,7 @@ import (
 	"sync"
 
 	mt "github.com/cbergoon/merkletree"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/golang/protobuf/jsonpb"
 	"golang.org/x/crypto/sha3"
@@ -365,6 +366,16 @@ func (b *Bloom) add(d []byte, buf []byte) {
 	b[i1] |= v1
 	b[i2] |= v2
 	b[i3] |= v3
+}
+
+// MarshalText encodes b as a hex string with 0x prefix.
+func (b Bloom) MarshalText() ([]byte, error) {
+	return hexutil.Bytes(b[:]).MarshalText()
+}
+
+// UnmarshalText b as a hex string with 0x prefix.
+func (b *Bloom) UnmarshalText(input []byte) error {
+	return hexutil.UnmarshalFixedText("Bloom", input, b[:])
 }
 
 // Test checks if the given topic is present in the bloom filter
