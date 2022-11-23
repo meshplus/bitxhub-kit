4519b8b90d90ee5032ecc86615cf5a5105335b30
Ruoxin <liruoxin@hyperchain.cn>
2022-11-23 11:53:36 +0800

    fix: btcd version

--- crypto/asym/ecdsa/public_key.go
+++ crypto/asym/ecdsa/public_key.go
@@ -7,7 +7,7 @@ import (
 	"encoding/asn1"
 	"fmt"
 
-	"github.com/btcsuite/btcd/btcec"
+	btcec "github.com/btcsuite/btcd/btcec/v2"
 	"github.com/meshplus/bitxhub-kit/crypto"
 	"github.com/meshplus/bitxhub-kit/types"
 )
